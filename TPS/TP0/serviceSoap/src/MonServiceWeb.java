4833527804760fdf356c2c8c0270d7ccc3c9948a
hugo-fm-edu <hugo.ferreiramiguel@edu.univ-paris13.fr>
2025-11-22 17:29:18 +0100

    Added TP0 files and specified ignored files

---
+++ TPS/TP0/serviceSoap/src/MonServiceWeb.java
@@ -0,0 +1,24 @@
+// SOAP : Simple Object Access Protocol
+// JAX-WS (Java API for XML Web Services)
+// JAXB (Java Architecture for XML Binding)
+
+// URL : Uniform Resource Locator > l'adresse de la ressource
+// URN : Uniform Resource Name > la ressource elle-même
+// URI : Uniform Resource Identifier > URL + URN
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://polytech.fr")
+public class MonServiceWeb {
+
+    @WebMethod(operationName = "convertir")
+    public double conversion(double mt){
+        return mt*0.9;
+    }
+
+    public double somme(@WebParam(name = "parametre1") double a, double b){
+        return a+b;
+    }
+}
